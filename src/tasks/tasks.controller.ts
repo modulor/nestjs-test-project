8f1d4ef6bbd7460852c1146a2bfed81e7ac662d6
Mario Lopez <modulor@hotmail.es>
2021-06-14 13:50:43 -0500

    crear un endpoint tipo PUT con un body y un parametro

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Delete, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Delete,
+  Param,
+  Put,
+} from '@nestjs/common';
 
 import { CreateTaskDTO } from './dto/create-task.dto';
 
@@ -30,6 +38,22 @@ export class TasksController {
     return {
       success: true,
       message: `Task id ${id} deleted successfully`,
+    };
+  }
+
+  @Put(':id')
+  updateTask(
+    @Body() task: CreateTaskDTO,
+    @Param('id') id,
+  ): {
+    success: boolean;
+    message: string;
+  } {
+    console.log('body: ', task);
+
+    return {
+      success: true,
+      message: `Task id ${id} updated successfully`,
     };
   }
 }
