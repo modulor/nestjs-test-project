fca2b104d1a9b2437149d7b23bd518ac5f498c3f
Mario Lopez <modulor@hotmail.es>
2021-06-14 18:29:08 -0500

    mover import de acuerdo a la jerarquia segun yo

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { TaskSchema } from './schemas/task.schema';
 
 @Module({
